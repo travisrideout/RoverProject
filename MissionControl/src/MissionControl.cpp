ef0b6e3df7cf9f965834a999a3bf9f198ecc9171
travisrideout <Travis.Rideout518@gmail.com>
2015-01-20 22:36:53 -0500

    in process of add readmedding text/GUI to MissionControl

--- MissionControl/src/MissionControl.cpp
+++ MissionControl/src/MissionControl.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <stdio.h>			// For _sleep()
 #include <conio.h>
-#include <SDL2/SDL.h>
+#include <SDL2/SDL.h>		// for controller input and windows/GUI
+#include <SDL2/SDL_ttf.h>	// for text on screen
 #include <windows.h>		// For Sleep()
 #include <Rover5Coms.h>
 #include "CollisionAvoidance.h"
@@ -16,17 +17,35 @@ int main(int argc, char *argv[]) {
 	SDL_Window *window = NULL;
 	SDL_Surface *windowSurface = NULL;
 	SDL_Surface *currentImage = NULL;
+	SDL_Renderer *renderTarget = NULL;
 
-	SDL_Init( SDL_INIT_VIDEO | SDL_INIT_JOYSTICK );  //
+	SDL_Init( SDL_INIT_VIDEO | SDL_INIT_JOYSTICK );
+	if(TTF_Init()<0){
+		std::cerr << "Failed to initialize SDL ttf" << std::endl;
+	}
 	SDL_Joystick *joystick = SDL_JoystickOpen(0);
 
 	window = SDL_CreateWindow("Rover5", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+	renderTarget = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
 	windowSurface = SDL_GetWindowSurface(window);
 	currentImage = SDL_LoadBMP("Screen.bmp");
 
 	std::cout << "Controller Name: " << SDL_JoystickName(joystick) << std::endl;
 	std::cout << "Num axes: " << SDL_JoystickNumAxes(joystick) << std::endl;
 	std::cout << "Num Buttons: " << SDL_JoystickNumButtons(joystick) << std::endl;
+
+	TTF_Font *font = TTF_OpenFont("d7_mono.ttf", 20);
+	SDL_Color color = {14,77,255,255};
+	SDL_Surface *textSurface = TTF_RenderText_Solid(font, "test text",color);
+	SDL_Texture *text = SDL_CreateTextureFromSurface(renderTarget,textSurface);
+	SDL_Rect textRect;
+	textRect.x = textRect.y = 0;
+
+	SDL_QueryTexture(text, NULL, NULL, &textRect.w, &textRect.h);
+
+	SDL_FreeSurface(textSurface);
+	textSurface = NULL;
 
 	SDL_Event ev;
 
@@ -36,6 +55,7 @@ int main(int argc, char *argv[]) {
 		Sleep(30);		// Sleep for 30ms to reduce CPU usage
 		SDL_BlitSurface(currentImage, NULL, windowSurface, NULL);
 		SDL_UpdateWindowSurface(window);
+		SDL_RenderCopy(renderTarget, text, NULL, &textRect);
 		server.GetSendMessageVars(&scratch_vars);
 
 		while(SDL_PollEvent(&ev) != 0){
@@ -87,6 +107,10 @@ int main(int argc, char *argv[]) {
 	SDL_DestroyWindow(window);
 	window = NULL;
 	windowSurface = NULL;
+	SDL_DestroyRenderer(renderTarget);
+	renderTarget = NULL;
+	SDL_DestroyTexture(text);
+	text = NULL;
 	SDL_Quit();
 
 	std::cout << "end program" << std::endl;
